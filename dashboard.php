e9a05e2f08240bd6cb78f886723648df86cfa06e
Hygor Henrique <hygor.henrique@live.com>
2023-02-27 14:12:14 -0300

    Updates

--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,29 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard - Odontocloud</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odontocloud - Painel</title>
+    <link rel="icon" type="image/png" href="Public/Assets/Images/oc_favicon.ico" sizes="16x16">
     <link rel="stylesheet" type="text/css" href="Public/Assets/Stylesheets/style.dashboard.css">
+    <link rel="stylesheet" type="text/css" href="Public/Assets/Semantic-ui/semantic.min.css">
+    <link rel="stylesheet" type="text/css" href="Public/Assets/Toastr/toastr.min.css">
+    <script type="text/javascript" src="Public/Assets/JQuery/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="Public/Assets/Semantic-ui/semantic.min.js"></script>
+    <script type="text/javascript" src="Public/Assets/Toastr/toastr.min.js"></script>
+    <script>let url = "http://localhost:8080/";toastr.options = {"closeButton": true, "debug": false, "newestOnTop": false, "progressBar": true, "positionClass": "toast-top-center", "preventDuplicates": false, "onclick": null, "showDuration": "300", "hideDuration": "1000", "timeOut": "5000", "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn", "hideMethod": "fadeOut"}</script>
 </head>
-<body>
 
+<body>
+    <div class="container">
+        <div class="sidenav">
+            <div class="logo"></div>
+        </div>
+
+        <div class="main">
+            <div class="top">
+
+            </div><!--top-->
+        </div>
+    </div>
 </body>
 </html>
